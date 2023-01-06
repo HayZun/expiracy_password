c73ff3706be1b25c5e916e5be2fd9250c01f0975
HayZun <58841936+HayZun@users.noreply.github.com>
2023-01-06 16:39:26 +0100

    Update expiry_password.ps1

--- expiry_password.ps1
+++ expiry_password.ps1
@@ -2,7 +2,9 @@ function getusers {
     param (
         $CN
     )
+    #Recupere les utilisateurs actifs ayant le champ adresse-email remplie
     $users = Get-Aduser -properties Name, PasswordNeverExpires, PasswordExpired, PasswordLastSet, EmailAddress -filter * | Where-Object { ($_.DistinguishedName -match $CN) -and ($_.EmailAddress -ne $null) -and ($_.PassWordLastSet -ne $null)}
+    #Retourne un tableau contenant les utilisateurs
     return $users
 }
 
@@ -10,14 +12,23 @@ function treatment {
     param (
             $data
     )
+    #Je stocke la date actuel
     $today = (get-date)
+    #Je stocke l'âge maximal du mot de passe définit dans la GPO 'Default Domain Policy'
     $maxPasswordAge = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge
 
+    #Je parcours mon tableau d'utilisateur
     foreach ($user in $data) {
+        #Je stocke dans un tableau le nom et le prénom de l'utilisateur
         $Name = (Get-ADUser $user | ForEach-Object { $_.Name }) -split (" ")
+        #Je recupere la date de la dernière modification du mot de passe de l'utilisateur
         $passwordsetdate = $user.passwordLastSet
+        #Je calcule la date d'expiration du mot de passe
         $expiresOn = $passwordsetdate + $maxPasswordAge
+        #Je convertie la date calculé en jours.
         $daystoexpire = -((New-TimeSpan -Start $expiresOn -End $today).Days)
+        
+        #J'initialise un boolean qui me servira à savoir si j'envoie ou non le mail
         $dayson = $false
 
         Switch ($daystoexpire) {
@@ -44,32 +55,48 @@ function treatment {
              }
         default {}
          }
+
+         # si la date d'expiration est égalle à 1,2,3,4,5,15,30 ou inférieur à 0, j'envoie un mail.
         if($dayson -or ($daystoexpire -lt 0)) {
+            #Je stocke le prenom de l'utilisateur
             $prenom = $Name[1]
+            #Je stocke l'adresse email de l'utilisateur
             $emailaddress = $user.emailaddress
 
-            $MailFrom = "XXXXXXXXXX"
+            #Renseignez ici l'emetteur du mail
+            $MailFrom = "HCPW20@humansconnexion.com"
+            #Renseignez ici le destinateur du mail ici l'utilisateur
             $MailTo  = $emailaddress
 
-            # Server Info
+            # Serveur Info
             $SmtpServer = "mail.humans.local"
             $SmtpPort = "25"
 
-            # Message stuff
+            #Je renseigne ici une partie du sujet du mail
             $subjectexpire = "expire dans $daystoexpire"
+            #message utilisé dans le corps du mail
             $messexpire = "expire dans <b>$daystoexpire</b>"
-            $abs = $day
+
+            #si la date d'expiration est inferieur à 0, je change le sujet et un bout de message utilisé 
             if($daystoexpire -lt 0) {
-                $abs = $daystoexpire * -1
-                $subjectexpire = "a déjà expiré depuis $abs"
-                $messexpire = "a déjà expiré depuis <b>$abs</b>"
+                #vu que la date d'expiration est un chiffre/nombre negatif, je le convertie en nombre positif
+                $absday = $daystoexpire * -1
+                #je modifie la partie du sujet du mail
+                $subjectexpire = "a déjà expiré depuis $absday"
+                #je modifie un bout de message utilise dans le corps
+                $messexpire = "a déjà expiré depuis <b>$absday</b>"
             }   
 
+            # je créé le sujet du mail
             $subject = "Votre mot de passe Windows $subjectexpire jours" 
+            #je créé le corps du mail
             $Message = New-Object System.Net.Mail.MailMessage $MailFrom,$MailTo
+            #j'active l'HTML dans le corps du mail
             $Message.IsBodyHTML = $true
+            #Je renseigne le sujet du mail
             $Message.Subject = $subject
-               
+            
+            #Je créé le corps du mail   
             $Message.Body = @"
 
             <p>Bonjour <b>$prenom</b>,</p>
@@ -95,8 +122,9 @@ function treatment {
             Le support de Data-expertise
             </p
 "@
-            # Construct the SMTP client object, credentials, and send
+            #Je créé l'objet me permettant d'envoyer le mail
             $Smtp = New-Object Net.Mail.SmtpClient($SmtpServer,$SmtpPort)
+            #J'envoie le mail à l'utilisateur
             $Smtp.Send($Message)
         }
     }
@@ -104,15 +132,15 @@ function treatment {
 
 function main {
     param (
-        $expiracydays,
         $CN
     )
-
+    #Recupere les utilisateurs actifs ayant le champ adresse-email remplie
     $data = getusers($CN)
     
     treatment($data)
 }
 
-#VARS
-$CN = "OU=Admins,OU=Utilisateurs,DC=XXX,DC=XXX"
-main($expiracydays, $CN)
+#VAR
+#Insérez le chemin où se trouve les utilisateurs
+$CN = "OU=Admins,OU=Utilisateurs,DC=humans,DC=local"
+main($CN) treatment($data)
